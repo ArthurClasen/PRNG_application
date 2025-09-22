2fd9c344e2d031abae9f85090e530dd7c30ca68b
clasen <arthurclasendemelo@gmail.com>
2025-09-21 23:05:09 -0300

    commmit

---
+++ src/test/java/prng/app/prng_application/PrngApplicationTests.java
@@ -0,0 +1,13 @@
+package prng.app.prng_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
